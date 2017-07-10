77cc8773cd86a3112e0f647fdf257b1573947c7a
Nin <therealnin@users.noreply.github.com>
2017-07-10 13:11:44 -0400

    Initial version of the script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Siege++Collection
+The collection of Siege++ mods that make the NS2 Siege++ project. [Check out the wiki](https://github.com/TheRealNin/Siege-Collection/wiki) for more information and discussion.
+
+* [Siege++ core](https://github.com/TheRealNin/SiegePlusPlus)
+* [Siege++ balance](https://github.com/TheRealNin/SiegeBalance)
---
+++ copy_mods.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+dest="./output/"
+
+cp -R ../EggSpawn/output/* $dest
+cp -R ../HadesDevice/output/* $dest
+cp -R ../ModPanels/output/* $dest
+cp -R ../ThirdPerson/output/* $dest
+cp -R ../WhipRebalance/output/* $dest
+cp -R ../MineMadness/output/* $dest
+cp -R ../ModularExos/output/* $dest
+cp -R ../NinS2Plus/output/* $dest
+cp -R ../SiegePlus/output/* $dest
+cp -R ../SiegeBalance/output/* $dest
+cp -R ../sg_trimsiege/output/* $dest
